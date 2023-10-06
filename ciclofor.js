50caf67e386bb6ac9889bc9d2bc7a8eb1a499de4
Anse98 <lorenzo.anselmi98.com>
2023-10-06 15:45:32 +0200

    ADD-Bonus 1

--- ciclofor.js
+++ ciclofor.js
@@ -1,19 +1,34 @@
+const container = document.getElementById("container");
+console.log(container)
+
 // Stampo in console i numeri da 1 a 100
 for (let i = 0; i < 100; i++ ) {
   let n = i + 1;
   //Stampo FizzBuzz SE entrambe le condizioni sono soddisfatte
   if (n % 3 === 0 && n % 5 === 0) {
-    console.log("FizzBuzz");
+    const node = document.createElement("div");
+    let text = document.createTextNode("FizzBuzz");
+    node.appendChild(text);
+    container.appendChild(node);
   //Stampo Buzz SE la variabile n è un multiplo di 5
   } else if (n % 5 === 0) {
-    console.log("Buzz");
+    const node = document.createElement("div");
+    let text = document.createTextNode("Buzz");
+    node.appendChild(text);
+    container.appendChild(node);
   //Stampo Fizz SE la variabile n è un multiplo di 3
   } else if (n % 3 === 0) {
-    console.log ("Fizz");
+    const node = document.createElement("div");
+    let text = document.createTextNode("Fizz");
+    node.appendChild(text);
+    container.appendChild(node);
   }
   //ALTRIMENTI stampo il valore di n
    else {
-    console.log(n);
+    const node = document.createElement("div");
+    let text = document.createTextNode(n);
+    node.appendChild(text);
+    container.appendChild(node);
   }
 }
 
